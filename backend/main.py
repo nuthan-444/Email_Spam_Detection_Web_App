4e3cfe1740812ee3eb78e0220d523f5e6c62ad69
NuthaN-444 <nuthanprasadkg@gmail.com>
2026-09-18 06:44:46 +0530

    Vercel.json added

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,18 @@
+from dotenv import load_dotenv
+import os
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 import joblib
 
+load_dotenv()
+
+api_key = os.getenv('API_KEY')
+
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[api_key,"http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
